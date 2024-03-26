d4356f6b77944497e8de0bbd5fd8588aa4eda8aa
Nin <nin@Nins-MacBook-Pro.local>
2024-03-26 14:31:40 +0700

    nhat hao

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ const Movie = require('./models/Trees.model');
 const User = require('./models/Users.model');
 const redis = require('./redis');
 
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+
 // Tree
 
 app.get('/api/tree', async (req, res) => {
