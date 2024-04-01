67964f32c9a9bc4ac6cae6cd941fec6505f26502
Nin <nin@Nins-MacBook-Pro.local>
2024-04-01 19:41:27 +0700

    a

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const redis = require('./redis');
 
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://main--webcaycanh.netlify.app/');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
