4a7438f340e01f608831fe1b5cb527734a3ec757
Le Nhat Hao <lenhathao280302@gmail.com>
2024-04-05 00:49:21 +0700

    m

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const redis = require('./redis');
 app.use(express.json());
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://main--webcaycanh.netlify.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://webcaycanh.netlify.app/');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
